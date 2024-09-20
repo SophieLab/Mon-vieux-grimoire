03cdb2224a0a60de92a58e3dd992c9e05ea68f79
Sophie Labyt <labytsophie@gmail.com>
2024-09-20 09:22:11 +0200

    modify controllers users and books

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -1,233 +1,103 @@
-const Book = require("../models/bookModel");
-const fs = require("fs");
-const mongoose = require('mongoose');
-const path = require("path");
+const Book = require('../models/Book');
+const fs = require('fs');
 
-//- GET : Récupération de tous les livres
-
-exports.getAllBooks = async (req, res) => {
-  //-Le but est ici de renvoyer un tableau avec tous les livres de la base de données
-  try {
-    const books = await Book.find();
-    res.status(200).json(books);
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+exports.getAllBooks = (req, res, next) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
 };
 
-//- POST : Création d'un livre
+exports.bestBooks = (req, res, next) => {
+  Book.find()
+    .sort({ averageRating: -1 })
+    .limit(3)
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+};
 
-exports.createBook = async (req, res) => {
-  if (!req.body.book || req.body.book === undefined || req.file === undefined) {
-    return res.status(400).json({ error: "Les données du livre ne sont pas valides" });
-  }
-  //-On stocke la requête demandée sous format JSON
+exports.getOneBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+  .then((book) => res.status(200).json(book))
+  .catch((error) => res.status(404).json({ error }));
+};
+
+exports.createBook = (req, res, next) => {
   const bookObject = JSON.parse(req.body.book);
-  //-On supprime le mauvais _id venant du front
   delete bookObject._id;
-  //-On supprime le "_userId" à qui on ne peut pas faire confiance
   delete bookObject._userId;
-  //-On créée l'instance "create" avec le modèle "Book"
   const book = new Book({
     ...bookObject,
     userId: req.auth.userId,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${
-      req.file.filename
-    }`,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
-  //-On enregistre le livre créé dans la base de données
-  try {
-    await book.save();
-    res.status(201).json({ message: "Livre enregistré avec succès !" });
-  } catch (error) {
-    res.status(400).json({ error });
-  }
+  book.save()
+    .then(() => res.status(201).json({ message: 'Livre enregistré !' }))
+    .catch((error) => res.status(400).json({ error }));
 };
 
-//- GET : Récupération d'un livre
-
-exports.getOneBook = async (req, res) => {
-
-  //-On vérifie si l'ID du livre est valide
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).json({ error: "Paramètre invalide !" });
-  }
-  
-  try {
-    const book = await Book.findOne({ _id: req.params.id });
-    if (book === null) {
-      return res.status(404).json({ error: "Ce livre n'existe pas" });
-    }
-    res.status(200).json(book);
-  } catch (error) {
-    res.status(404).json({ error: "Ce livre n'existe pas" });
-  }
-};
-
-//- PUT : Modification d'un livre
-
-exports.modifyBook = async (req, res) => {
-
-  //-On vérifie que l'ID donné est valide
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).json({ error: "Paramètre invalide !" });
-  }
-
-  if ( req.body.book === undefined && req.file === undefined && Object.keys(req.body).length === 0
-  ) {
-    return res.status(400).json({ error: "Les modifications ne sont pas valides" });
-  }
-
-  const bookObject = req.file
-    ? {
-        //-On stocke une nouvelle fois la requête demandée au format JSON
-        ...JSON.parse(req.body.book || "{}"),
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${
-          req.file.filename
-        }`,
-      }
-    : { ...req.body };
+exports.modifyBook = (req, res, next) => {
+  const bookObject = req.file ? {
+    ...JSON.parse(req.body.book),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
 
   delete bookObject._userId;
-
-  //-On vérifie si les données entrées sont valides
-  if (typeof bookObject.genre != 'string' || isNaN(bookObject.year)) {
-    return res.status(400).json({ error: "Les données fournies sont invalides" });
-  }
-
-  //-On recherche le livre demandé dans la base de données à l'aide de son ID
-  try {
-    const book = await Book.findOne({ _id: req.params.id });
-    //-On vérifie que le livre existe bien dans la base de données
-    if (!book) {
-      return res.status(404).json({ message: "Livre non trouvé !" });
-    }
-
-    //-Si le livre existe bien, on vérifie les autorisations de l'utilisateur connecté pour savoir si il peut modifier le livre choisi
-    if (book.userId != req.auth.userId) {
-      return res.status(401).json({ message: "Requête non autorisée !" });
-    }
-
-    //-Une fois les autorisations vérifiées, on sauvegarde les nouvelles informations du livre
-    await Book.updateOne(
-      { _id: req.params.id },
-      { ...bookObject, _id: req.params.id }
-    );
-
-    if (!req.file) {
-      return res.status(200).json({ message: "Livre modifié avec succès !" });
-    }
-
-    const filename = book.imageUrl.split("/images/")[1];
-    fs.unlink(`images/${filename}`, () => {
-      res.status(200).json({ message: "Livre modifié avec succès !" });
-    });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-};
-
-//- DELETE : Supression d'un livre
-
-exports.deleteBook = async (req, res) => {
-  //-On vérifie que l'ID du book est valide
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).json({ error: "Paramètre invalide !" });
-  }
-  //-On recherche à nouveau le livre demandé dans la base de données à l'aide de son ID
-  try {
-    const book = await Book.findOne({ _id: req.params.id });
-    //-Si le livre n'est pas trouvé on renvoie une erreur 404
-    if (!book) {
-      return res.status(404).json({ error: "Ce livre n'existe pas" });
-    }
-    //-On vérifie les autorisations de l'utilisateur connecté pour savoir si il peut supprimer le livre choisi
-    if (book.userId != req.auth.userId) {
-      return res.status(403).json({ message: "Requête non autorisée !" });
-    }
-
-    const filename = book.imageUrl.split("/images/")[1];
-    fs.unlink(`images/${filename}`, async () => {
-      //-Une fois les autorisations vérifiées, on supprime le livre
-      try {
-        await Book.deleteOne({ _id: req.params.id });
-        res.status(200).json({ message: "Livre supprimé avec succès !" });
-      } catch (error) {
-        res.status(400).json({ error: "Impossible de supprimer ce livre" });
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (book.userId != req.auth.userId) {
+        res.status(403).json({ message: 'Requête non autorisée' });
+      } else {
+        Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Livre modifié !' }))
+        .catch(error => res.status(401).json({ error }));
       }
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
     });
-  } catch (error) {
-    res.status(500).json({ error: "Erreur lors de la mise à jour de la note du livre." });
-  }
 };
 
-//- GET : Obtenir les 3 livres ayant la meilleure note
-
-exports.bestRatings = async (req, res) => {
-  try {
-    const books = await Book.find().sort({ averageRating: -1 }).limit(3);
-    if (books.length === 0) {
-      //-Si on a pas de livres dans la base de données, on renvoie une erreur 404
-      return res.status(404).json({ error: "Aucun livre trouvé" });
-    }
-    res.status(200).json(books);
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-};
-
-//- POST : Mettre une note au livre et en calculer la moyenne
-
-exports.bookRating = async (req, res) => {
-  const { rating, userId } = req.body;
-
-    //-On vérifie si l'ID du livre est valide
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.status(400).json({ error: "Paramètre invalide !" });
-    }
-  //-On vérifie si le livre est présent dans la base de données
-  try {
-    const book = await Book.findOne({ _id: req.params.id });
-    if (!book) {
-      res.status(404).json({ error: "Aucun livre n'a été trouvé" });
-    }
-
-    //-On vérifie si l'utilisateur a déjà noté ce livre
-    for (let userRate of book.ratings) {
-      if (userRate.userId == userId) {
-        return res.status(409).json({ error: "L'utilisateur a déjà noté ce livre." });
+exports.deleteBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => {
+      if (book.userId != req.auth.userId) {
+        res.status(401).json({ message: 'Non autorisé' });
+      } else {
+        const filename = book.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Book.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Livre supprimé !' }))
+            .catch(error => res.status(401).json({ error }));
+        });
       }
-    }
+    })
+    .catch(error => res.status(500).json({ error }));
+};
 
-    //-On vérifie si le userId de l'utilisateur entré est le bon
-    if (req.auth.userId != userId) {
-      return res.status(400).json({ error: "Requête invalide !" });
-    }
+exports.rateBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => {
+      if (book.ratings.some(rating => rating.userId === req.auth.userId)) {
+        return res.status(400).json({ message: 'Vous avez déjà noté ce livre' });
+      };
 
-    //-On vérifie si la note entrée est correcte
-    if (rating < 0 || rating > 5 || !Number.isInteger(rating)) {
-      return res.status(400).json({ error: "La note n'est pas valide" });
-    }
-    //-On ajoute la note à l'array rating
-    book.ratings.push({
-      grade: rating,
-      userId: req.auth.userId,
-    });
-    //-On calcule la somme des notes, puis de la moyenne
-    const totalRatings = book.ratings.length;
-    const sumRatings = book.ratings.reduce(
-      (sum, rating) => sum + rating.grade,
-      0
-    );
-    const averageRating = sumRatings / totalRatings;
+      const newRating = {
+        userId: req.auth.userId,
+        grade: req.body.rating
+      };
 
-    //-On met à jour la moyenne des notes dans le livre
-    book.averageRating = averageRating;
+      book.ratings.push(newRating);
+      console.log(book.ratings);
 
-    //-On sauvegarde les modifications dans la base de données
-    const updatedBook = await book.save();
-    res.status(200).json(updatedBook);
-  } catch (error) {
-    res.status(500).json({ error: "Erreur lors de la notation du livre" });
-  }
+      const sumOfGrades = book.ratings.reduce((acc, currentValue) => acc + currentValue.grade, 0);
+      const nbOfRatings = book.ratings.length;
+      console.log(nbOfRatings, 'numberOfRatings')
+
+      book.averageRating = parseFloat((sumOfGrades / nbOfRatings).toFixed(1));
+
+      book.save()
+        .then(() => res.status(200).json(book))
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,45 +1,42 @@
-// Création de compte
-exports.signup = async (req, res) => {
-  //-On appelle la fonction hachage de bcrypt dans le mot de passe qui sera "salé" 10 fois
-  try {
-    const hash = await bcrypt.hash(req.body.password, 10);
-    //-On crée une instance à partir du modèle User
-    const user = new User({
-      email: req.body.email,
-      password: hash,
-    });
-    //-On enregistre l'utilisateur dans la base de donées
-    await user.save();
-    res.status(201).json({ message: "Utilisateur crée avec succès !" });
-  } catch (error) {
-    res.status(400).json({ error });
-  }
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
 
-//- Connexion
-exports.login = async (req, res) => {
-  //-On vérifie l'existence de l'utilisateur dans la base de données
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    
-    if (!user || !req.body.email || !req.body.password) {
-      return res.status(400).json({ message: "Requête invalide !" });
-    }
-
-    //-On compare le mot passe entré avec le hash qui est dans la base de données
-    const valid = await bcrypt.compare(req.body.password, user.password);
-    if (!valid) {
-      return res.status(401).json({ message: "Association identifiant/mot de passe incorrecte !" });
-    }
-
-    //-Si les informations sont valides, on renvoie une réponse contenant le userId et un token jwt
-    res.status(200).json({
-      userId: user._id,
-      token: jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
-        expiresIn: "24h",
-      }),
-    });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+      .then(user => {
+          if (!user) {
+              return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+          }
+          bcrypt.compare(req.body.password, user.password)
+              .then(valid => {
+                  if (!valid) {
+                      return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                  }
+                  res.status(200).json({
+                      userId: user._id,
+                      token: jwt.sign(
+                          { userId: user._id },
+                          'RANDOM_TOKEN_SECRET',
+                          { expiresIn: '24h' }
+                      )
+                  });
+              })
+              .catch(error => res.status(500).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
 };
