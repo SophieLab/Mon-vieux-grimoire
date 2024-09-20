403874c78c0f5c94fc53c24bf12ecc29a21c180e
Sophie Labyt <labytsophie@gmail.com>
2024-09-20 11:21:00 +0200

    refacto

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,35 @@ const app = express();
 // Connexion à la base de données MongoDB
 mongoose.connect(
   process.env.MONGODB_URI, // URI de la base de données MongoDB depuis les variables d'environnement
-)
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+ )
+   .then(() => console.log('Connexion à MongoDB réussie !'))
+   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://saiphye:osNOCOBMr1bcxFM0@cluster0.lbvbm.mongodb.net/saiphye?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+// run().catch(console.dir);
 
 app.use(express.json()); // Middleware pour parser les requêtes JSON
 
